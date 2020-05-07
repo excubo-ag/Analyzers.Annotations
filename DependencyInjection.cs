96965c57eeeee684e89c2fc877512471c48b2706
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-05-07 17:17:19 +0200

    initial commit

---
+++ DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Excubo.Analyzers.DependencyInjection
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class ExposesAttribute : Attribute
+    {
+        public ExposesAttribute(Type type)
+        {
+        }
+    }
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class AsAttribute : Attribute
+    {
+        public AsAttribute(Type @interface)
+        {
+        }
+    }
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class IgnoreDependencyAttribute : Attribute
+    {
+        public IgnoreDependencyAttribute(params Type[] interfaces)
+        {
+        }
+    }
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class DependencyInjectionPointAttribute : Attribute
+    {
+    }
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class InjectsAttribute : Attribute
+    {
+        public InjectsAttribute(params Type[] types)
+        {
+        }
+    }
+}
